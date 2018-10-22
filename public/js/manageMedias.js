4f5e145d0e3490cf1684decbfda67c8b884130da
dyl25 <dylan-vst@hotmail.com>
2018-10-22 11:51:09 +0200

    change manage media js

--- public/js/manageMedias.js
+++ public/js/manageMedias.js
@@ -1,8 +1,12 @@
 console.log('manageMedias');
 
 let $btDeletMedia = document.getElementById('btDeleteMedia');
-
+fetch('/api/medias').then(response => console.log(response));
 //$btDeletMedia.onclick;
+
+function loadMedias() {
+    fetch('/api/medias').then(response => console.log(response));
+}
 
 function deleteMedia(id) {
     fetch('/api/medias', {
