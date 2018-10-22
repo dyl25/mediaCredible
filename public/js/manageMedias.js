ad711ac19aa5756973ff194a27c0246b072ce80f
dyl25 <dylan-vst@hotmail.com>
2018-10-22 10:52:15 +0200

    implementation of delete medias in js

--- public/js/manageMedias.js
+++ public/js/manageMedias.js
@@ -1,1 +1,16 @@
 console.log('manageMedias');
+
+let $btDeletMedia = document.getElementById('btDeleteMedia');
+
+//$btDeletMedia.onclick;
+
+function deleteMedia(id) {
+    fetch('/api/medias', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type' : 'application/json'
+        }
+    });
+}
+
+console.log($btDeletMedia);
